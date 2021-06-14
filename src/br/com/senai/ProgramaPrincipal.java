2141a25a6a7ca384e116a79c18d88b40ff7fcaf8
vinicios0202 <vinicius_serrano@estudante.sesisenai.org.br>
2021-06-14 16:41:49 -0300

    refatoração de classe

--- src/br/com/senai/ProgramaPrincipal.java
+++ src/br/com/senai/ProgramaPrincipal.java
@@ -1,14 +1,21 @@
-package br.com.senai.pessoa;
+package br.com.senai;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import br.com.senai.pessoa.Pessoa;
+import br.com.senai.pessoa.PessoaController;
+import br.com.senai.produto.Produto;
+import br.com.senai.produto.ProdutoController;
 
 public class ProgramaPrincipal {
 
 	public static void main(String[] args) {
 		List<Pessoa> pessoas = new ArrayList<>();
+		List<Produto> produtos = new ArrayList<Produto>();
 
 		PessoaController pessoaController = new PessoaController();
+		ProdutoController produtoController = new ProdutoController();
 
 		boolean sair = false;
 
@@ -25,13 +32,21 @@ public class ProgramaPrincipal {
 				case 2:
 					pessoaController.listarPessoas(pessoas);
 					break;
+					
+				case 3:
+					produtos.add(produtoController.cadastrarProduto());
+					break;
+					
+				case 4:
+					produtoController.listarProdutos(produtos);
+					break;
 
 				case 9:
 					sair = true;
 					break;
 
 				default:
-					System.out.println("Op√ß√£o inv√°lida!");
+					System.out.println("OpÁ„o Invalida!");
 					break;
 			}
 		}while(!sair);
